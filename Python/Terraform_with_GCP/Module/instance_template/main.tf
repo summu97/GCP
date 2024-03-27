9f8f70d77333d85379a7837f4de47b045685bbf6
summu97 <135579543+summu97@users.noreply.github.com>
2024-03-27 22:10:00 +0530

    Update main.tf

--- Python/Terraform_with_GCP/Module/instance_template/main.tf
+++ Python/Terraform_with_GCP/Module/instance_template/main.tf
@@ -1,3 +1,5 @@
+NOTE: when you are playing with template make sure your main.tf file have the same code because when you are defining to take values from other modules, it means you are creating resources from that modules.if you ignored this and run the normal way it will throw 409 error.
+----------------------------------------------
 provider "google" {
   project     = "ringed-hallway-417305"
   region      = "us-east1-b"
